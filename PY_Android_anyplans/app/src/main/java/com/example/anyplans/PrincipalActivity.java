2e93dc6ea9dfc202145230b1abe1a5d0ad2dd0bf
maicolm <sistemas_mr@hotmail.com>
2025-05-28 14:48:09 -0500

    Se agrega el proyecto

---
+++ PY_Android_anyplans/app/src/main/java/com/example/anyplans/PrincipalActivity.java
@@ -0,0 +1,39 @@
+package com.example.anyplans;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PrincipalActivity extends AppCompatActivity {
+    Button btnPrueba;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_principal);
+
+        btnPrueba = findViewById(R.id.btnPrueba);
+
+        btnPrueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Tu mensaje aquí", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
